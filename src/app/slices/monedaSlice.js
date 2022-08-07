e9a4cc3c0b2a0aa0ae74633c33cf76b6087f8e8f
Nicol1994 <103080943+Nicol1994@users.noreply.github.com>
2022-08-07 16:23:28 -0300

    crear transaccion

--- src/app/slices/monedaSlice.js
+++ src/app/slices/monedaSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   moneda:[],
+  monedaCot: null,
 };
 
 export const monedaSlice = createSlice({
@@ -13,9 +14,14 @@ export const monedaSlice = createSlice({
       const { payload } = action;
       state.moneda = payload;
     },
+    setMonedaCot:(state, action) => {
+        const { payload } = action;
+        state.monedaCot = payload;
+      },
+    
     
   },
 });
 
-export const { setMoneda } = monedaSlice.actions;
+export const { setMoneda, setMonedaCot } = monedaSlice.actions;
 export default monedaSlice.reducer;
--- src/components/Screens/Dashboard/Main/CreateTrans/CreateTrans.jsx
+++ src/components/Screens/Dashboard/Main/CreateTrans/CreateTrans.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getMonedas } from '../../../../../services/crypto'
 import { useRef, useEffect } from 'react'
-import { setMoneda } from '../../../../../app/slices/monedaSlice'
+import { setMoneda} from '../../../../../app/slices/monedaSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import Button from '../../../../UI/Button/Button'
 import Select from '../../../../UI/Select/Select'
@@ -12,20 +12,28 @@ const selTrans = useRef()
 const monedas = useSelector(state => state.moneda.moneda)
 const user = useSelector(state => state.user.user)
 const inputCantidad = useRef()
+const valor = useRef()
+
 const dispatch = useDispatch()
 console.log(user)
   useEffect(() => {
     try {
       ;(async () => {
         const { monedas } = await getMonedas(user.apiKey)
+        console.log(monedas)
         dispatch(setMoneda(monedas))
       })()
     } catch (error) {}
   }, [dispatch])
-  const valorActual = () => {
-    return(
-      monedas.map()
-    )
+  const _onHandleMonedahange = async idMoneda => {
+    try {
+      console.log(idMoneda)
+      const { monedas } = await getMonedas(user.apiKey)
+      const mostrar = monedas.filter( moneda => moneda.id === Number(idMoneda))
+      console.log(mostrar)
+      
+      
+    } catch (error) {}
   }
   const onHandle= async e => {
     e.preventDefault()
@@ -59,18 +67,16 @@ console.log(user)
         </select>
         <br />
         <label>Seleccionar Moneda:</label>
-        <Select options={monedas}/>
+        <Select options={monedas}  onHandleChange={_onHandleMonedahange} />
         <br />
         <label>Cantidad:</label>
         <br />
         <input className='form-control' type='text' ref={inputCantidad} />
         <br />
         <label>Valor actual:</label>
-        <p className='card-text'>
-                <span className='badge bg-danger'>
-                  {valorActual()}
-                </span>
-              </p>
+        <ul>
+          <li value={valor}></li>
+        </ul>
         <Button
           cta='crearTrans'
           classColor={'btn-primary'}
